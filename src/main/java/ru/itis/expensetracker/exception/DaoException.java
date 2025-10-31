620a093d26ebf6a0af5c9ccfe47e02ba1a180c0b
GoshaPozhik <georg007722@gmail.com>
2025-10-30 20:15:39 -0400

    feat: add an exceptions

---
+++ src/main/java/ru/itis/expensetracker/exception/DaoException.java
@@ -0,0 +1,7 @@
+package ru.itis.expensetracker.exception;
+
+public class DaoException extends RuntimeException {
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
