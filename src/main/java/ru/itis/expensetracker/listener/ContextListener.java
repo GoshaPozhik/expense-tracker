2cd793169e78a1ee7ba32c31db3b03c93d868741
GoshaPozhik <georg007722@gmail.com>
2025-11-06 13:14:15 -0500

    feat(listener): add a listener

---
+++ src/main/java/ru/itis/expensetracker/listener/ContextListener.java
@@ -0,0 +1,47 @@
+package ru.itis.expensetracker.listener;
+
+import ru.itis.expensetracker.dao.CategoryDao;
+import ru.itis.expensetracker.dao.ExpenseDao;
+import ru.itis.expensetracker.dao.UserDao;
+import ru.itis.expensetracker.dao.WalletDao;
+import ru.itis.expensetracker.dao.impl.JdbcCategoryDao;
+import ru.itis.expensetracker.dao.impl.JdbcExpenseDao;
+import ru.itis.expensetracker.dao.impl.JdbcUserDao;
+import ru.itis.expensetracker.dao.impl.JdbcWalletDao;
+import ru.itis.expensetracker.service.AuthService;
+import ru.itis.expensetracker.service.WalletService;
+import ru.itis.expensetracker.service.impl.AuthServiceImpl;
+import ru.itis.expensetracker.service.impl.WalletServiceImpl;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext servletContext = sce.getServletContext();
+
+        // Создаем экземпляры DAO
+        UserDao userDao = new JdbcUserDao();
+        WalletDao walletDao = new JdbcWalletDao();
+        CategoryDao categoryDao = new JdbcCategoryDao();
+        ExpenseDao expenseDao = new JdbcExpenseDao();
+
+        // Создаем экземпляры сервисов, внедряя в них DAO
+        AuthService authService = new AuthServiceImpl(userDao, walletDao);
+        WalletService walletService = new WalletServiceImpl(walletDao, expenseDao, categoryDao, userDao);
+
+        // Сохраняем сервисы в ServletContext, чтобы они были доступны всем сервлетам
+        servletContext.setAttribute("authService", authService);
+        servletContext.setAttribute("walletService", walletService);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // Здесь можно освобождать ресурсы, если это необходимо
+    }
+}
